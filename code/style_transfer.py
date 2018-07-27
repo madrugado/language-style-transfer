882246c7c03fd94f8978c2fcf8bf18cb84915612
Valentin Malykh <madrugado@yandex.ru>
2018-07-27 19:24:00 +0300

    CHNG: computing gradients only for grad

--- code/style_transfer.py
+++ code/style_transfer.py
@@ -151,7 +151,8 @@ class Model(object):
         self.grad_adv_norm = tf.global_norm(grad_adv)
         self.grad_norm = tf.global_norm(grad)
 
-        self.optimize_tot = opt.apply_gradients(list(zip(grad, theta_eg)))
+        # self.optimize_tot = opt.apply_gradients(list(zip(grad, theta_eg)))
+        self.optimize_tot = opt.apply_gradients(grad)
         self.optimize_rec = opt.minimize(self.loss_rec, var_list=theta_eg)
         self.optimize_d0 = opt.minimize(self.loss_d0, var_list=theta_d0)
         self.optimize_d1 = opt.minimize(self.loss_d1, var_list=theta_d1)
