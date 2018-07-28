458de838babc302b25baf143d6c554705a2c1287
Valentin Malykh <madrugado@yandex.ru>
2018-07-28 11:29:25 +0300

    ENH: split

--- code/style_transfer.py
+++ code/style_transfer.py
@@ -168,10 +168,11 @@ def transfer(model, decoder, sess, args, vocab, data0, data1, out_path):
     losses = Accumulator(len(batches), ['loss', 'rec', 'adv', 'd0', 'd1'])
     for batch in batches:
         rec, tsf = decoder.rewrite(batch)
+        half = len(tsf)//2
         # data0_rec += rec[:half]
         # data1_rec += rec[half:]
-        tsf1, tsf2 = tf.split(tsf, 2)
-        data0_tsf += tsf1
+        tsf1, tsf2 = tsf[:half], tsf[half:]
+        data0_tsf += tsf
         data1_tsf += tsf2
 
         loss, loss_rec, loss_adv, loss_d0, loss_d1 = sess.run([model.loss,
